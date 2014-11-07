0d00893876b39116be01a8b7d450916d834b08cb
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-11-07 17:45:31 -0500

    placing circles on svg at random locations

--- watchout.js
+++ watchout.js
@@ -1,9 +1,31 @@
 // start slingin' some d3 here.
-$(document).ready(function() {
-  var height = 960;
-  var width = 500;
+window.onload = function() {
+  var width = 960;
+  var height = 500;
 
   var svg = d3.select('body').append('svg')
               .attr('width', width)
               .attr('height', height);
-});
+
+  var nodes = d3.range(5)
+                .map(function() {
+
+                  return {
+
+                    radius: 10,
+
+                    x: Math.random() * width,
+
+                    y: Math.random() * height
+
+                  };
+
+                });
+
+  svg.selectAll('circle').data(nodes)
+                         .enter()
+                         .append('circle')
+                         .attr('r', function(d) { return d.radius; })
+                         .attr('cx', function(d) {return d.x; })
+                         .attr('cy', function(d) {return d.y; })
+};
