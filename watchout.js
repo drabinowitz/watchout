7f89b310aada8c0bcff476f4e0fb16718cfecfbc
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-11-07 18:10:27 -0500

    putting player dot on svg

--- watchout.js
+++ watchout.js
@@ -7,7 +7,7 @@ window.onload = function() {
               .attr('width', width)
               .attr('height', height);
 
-  var nodes = d3.range(5)
+  var nodes = d3.range(6)
                 .map(function() {
                   return {
                     radius: 10,
@@ -16,12 +16,25 @@ window.onload = function() {
                   };
                 });
 
-  var circles = svg.selectAll('circle').data(nodes)
+  var player = nodes[0];
+
+  player.x = width/2;
+  player.y = height/2;
+
+  var circles = svg.selectAll('circle').data(nodes.slice(1))
                          .enter()
                          .append('circle')
                          .attr('r', function(d) { return d.radius; })
                          .attr('cx', function(d) {return d.x; })
                          .attr('cy', function(d) {return d.y; });
+
+  var playerCircle = svg.selectAll('.player').data([player])
+                    .enter()
+                    .append('circle')
+                    .attr('class','player')
+                    .attr('r', function(d) { return d.radius; })
+                    .attr('cx', function(d) {return d.x; })
+                    .attr('cy', function(d) {return d.y; });
 
   setInterval(function() {
     circles.transition()
